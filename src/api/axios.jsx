8aeb295109b584d5b2465c29de36dbacca6a3156
mohdfaizanahmed <mfaizancse@gmail.com>
2025-03-29 15:20:41 +0530

    newcode

---
+++ src/api/axios.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API = axios.create({
+  // baseURL: "https://hostel-management-backend-g35l.onrender.com/api/auth", //backend url
+  
+    baseURL: "http://localhost:5000/api/auth", // Use your local server
+  
+  
+});
+
+//Add token to request headers
+API.interceptors.request.use((config) => {
+  const userData =JSON.parse(localStorage.getItem("userData"));
+  console.log(userData)
+  const token = userData?.token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default API;
